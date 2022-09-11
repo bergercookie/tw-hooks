e2afcba9372b87b6c404927147ef0441918f3b97
Nikos Koukis <nickkouk@gmail.com>
2022-09-11 17:42:15 +0100

    [MISC] Remove unimplemented new modules

--- tw_hooks/hooks/__init__.py
+++ tw_hooks/hooks/__init__.py
@@ -8,8 +8,6 @@ def import_concrete_hooks():
     from .auto_tag_based_on_tags import AutoTagBasedOnTags
     from .correct_tag_names import CorrectTagNames
     from .detect_mutually_exclusive_tags import DetectMutuallyExclusiveTags
-    from .git_commit import GitCommit
-    from .handle_tag_bundles import HandleTagBundles
     from .post_latest_start_to_i3_status import PostLatestSTartToI3Status
     from .warn_on_task_congestion import WarnOnTaskCongestion
 
